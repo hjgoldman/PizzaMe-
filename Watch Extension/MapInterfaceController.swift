4218921d62e0cf70972c84f06046f1d6f5028580
hjgoldman <haydengoldman@gmail.com>
2017-05-01 12:09:01 -0500

    Added some functionality as Tacoma watch

---
+++ Watch Extension/MapInterfaceController.swift
@@ -0,0 +1,58 @@
+//
+//  MapInterfaceController.swift
+//  PizzaMe!
+//
+//  Created by Hayden Goldman on 5/1/17.
+//  Copyright © 2017 Hayden Goldman. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import MapKit
+
+
+class MapInterfaceController: WKInterfaceController, CLLocationManagerDelegate {
+    @IBOutlet var map: WKInterfaceMap!
+    var locationManager = CLLocationManager()
+    var closestPizza: Location?
+    
+    
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+        
+        self.locationManager = CLLocationManager()
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.distanceFilter = kCLDistanceFilterNone
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+        
+        let coordinateSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let coordinateRegion = MKCoordinateRegion(center: (self.locationManager.location?.coordinate)!, span: coordinateSpan)
+        
+        map.setRegion(coordinateRegion)
+        
+        if let closestPizza = context as? Location {
+            self.closestPizza = closestPizza
+            let coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees((self.closestPizza?.locationLat)!), longitude: CLLocationDegrees((self.closestPizza?.locationLng)!))
+            map.addAnnotation(coordinate, withImageNamed: "pizza_marker_watch.png", centerOffset: CGPoint(x: 0, y: 0))
+        }
+        
+    }
+    
+    
+    
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+    
+}
